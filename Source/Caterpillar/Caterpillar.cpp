01fc5f46b41ba3148ec95fa63819130f31a6b7a8
fabsther <fabsther@gmail.com>
2020-02-09 17:40:02 -0800

    Initial Commit

---
+++ Source/Caterpillar/Caterpillar.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Caterpillar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Caterpillar, "Caterpillar" );
